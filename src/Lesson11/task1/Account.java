e98ae8b3ecde242db49080de4ed8949e3128dbe6
maopa <maopa@bk.ru>
2024-07-04 19:29:25 +0300

    Add Deal

--- src/Lesson11/task1/Account.java
+++ src/Lesson11/task1/Account.java
@@ -2,7 +2,9 @@ package Lesson11.task1;
 
 abstract class Account {
     private long accountSize;
+
     abstract boolean add(long amount);
+
     abstract boolean pay(long amount);
 
     abstract boolean transfer(Account account, long amount);
--- src/Lesson11/task1/Main.java
+++ src/Lesson11/task1/Main.java
@@ -3,8 +3,8 @@ package Lesson11.task1;
 public class Main {
 
     public static void main(String[] args) {
-        Account PiterSimple = new SimpleAccount(70_000L);
-        Account MariSimple = new SimpleAccount(80_000L);
+        Account PiterSimple = new SimpleAccount(100L);
+        Account MariSimple = new SimpleAccount(2000L);
         PiterSimple.add(2000L);
         PiterSimple.transfer(MariSimple,5000L);
 
