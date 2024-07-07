e0732836353c89f36eeff48c808dac71d58ddf23
maopa <maopa@bk.ru>
2024-07-07 13:26:13 +0300

    Add Deal

--- src/Lesson12/Main.java
+++ src/Lesson12/Main.java
@@ -1,8 +1,29 @@
 package Lesson12;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String verifyInput;
+        Scanner scanner = new Scanner(System.in);
+        PasswordChecker pass = new PasswordChecker();
 
+
+        System.out.println("Введите мин. длину пароля: ");
+        pass.setMinLength(Integer.parseInt(scanner.nextLine()));
+
+        System.out.println("Введите макс. допустимое количество повторений символа");
+        pass.setMaxRepeats(Integer.parseInt(scanner.nextLine()));
+
+        while (true) {
+            System.out.println("Введите пароль или end: ");
+            verifyInput = scanner.nextLine();
+            if ("end".equals(verifyInput)) {
+                break;
+            }
+
+            pass.verify(verifyInput);
+        }
 
     }
 
--- src/Lesson12/PasswordChecker.java
+++ src/Lesson12/PasswordChecker.java
@@ -1,4 +1,28 @@
 package Lesson12;
 
 public class PasswordChecker {
+    private int minLength;
+    private int maxRepeats;
+
+    public void setMinLength(int minLength) {
+        this.minLength = minLength;
+    }
+
+    public void setMaxRepeats(int maxRepeats) {
+        this.maxRepeats = maxRepeats;
+    }
+
+    public boolean verify(String password) {
+        int repeat = 0;
+        for (int i = 0; i < password.length() - 1; i++) {
+            if (password.charAt(i) == password.charAt(i + 1)) {
+                repeat++;
+            }
+        }
+        if (minLength > password.length() || repeat > maxRepeats) {
+            return false;
+        }
+
+        return true;
+    }
 }
